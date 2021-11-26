6553cdc2bc906febfc8040102073bc80bcc86939
Fernando Matamoros <fmatamoros202@gmail.com>
2021-11-26 08:07:03 -0500

    Initial folder and file structure for convunity project

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(server, {
+    cors:{
+        origin: '*',
+        methods: ['GET','POST']
+    }
+});
+
+app.use(cors());
+
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res)=>{
+    res.send('Server is running.');
+})
+
+io.on('connection', (socket)=>{
+    socket.emit('me', socket.id);
+
+    socket.on('disconnect', ()=>{
+        socket.broadcast.emit('')
+    })
+
+})
+
+server.listen(PORT, ()=>{
+    console.log(`Server listening on port ${PORT}`)
+})
