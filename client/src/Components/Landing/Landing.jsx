466350fd6ff09147c35e7e059f691547fd9b486e
Fernando Matamoros <fmatamoros202@gmail.com>
2021-12-03 13:37:11 -0500

    Finilized firt iteration of the landing page

--- client/src/Components/Landing/Landing.jsx
+++ client/src/Components/Landing/Landing.jsx
@@ -1,10 +1,11 @@
 import './Landing.scss';
 import {Link} from 'react-router-dom';
 import logo from '../../assets/logo/logo-main.png';
-import logoGirl from '../../assets/images/logo-girl.png';
-import quote from '../../assets/images/hero.png';
+// import logoGirl from '../../assets/images/logo-girl.png';
+import hero from '../../assets/images/hero.png';
 import quote2 from '../../assets/images/quote.png';
 import quote3 from '../../assets/images/quote2.png';
+import quote4 from '../../assets/images/quote4.png';
 
 function Landing(props) {
 
@@ -30,30 +31,36 @@ function Landing(props) {
             </div>
         </header>
         <div className="hero">
-            <img className="hero__image" src={quote}/>
+            <img className="hero__image" src={hero} alt="different chat bubbles"/>
             <div className="hero__container">
-                <h3>The best app for language fluency!</h3>
-                <p></p>
+                <h3>What's Convunity?</h3>
+                <p>Convunity is language fluency made easy!</p>
+                <p>Use convunity to have fast conversations with native speakers to help you supplement your language learning journey.</p>
+                <p>Choose the language you want to practice and your native language and voila! You are ready to practice and help others.</p>
                 <Link to='/register' className="link"><div>Sign up for free</div></Link>
             </div>
         </div>
         <section className="main">
             <div className="main__container">
-                <img className="main__container__image" src={quote}/>
-                <h3>What's Convunity</h3>
-                <p></p>
+                <img className="main__container__image" src={quote2} alt="quote bubble"/>
+                <h3>Convunity = Conversation and Community</h3>
+                <p>When coming up with the idea for Convunity we realised that learning a new language is all about becoming conversationally adept.</p>
+                <p>Yes, reading and writing are equally important, but the greatest satisfaction of learning a language comes from the ability to sustain conversations.</p>
+                <p>Convunity is an easy-to-use app that lets you have short conversations with native speakers in the language you are learning and practicing.</p>
                 <Link to='/register' className="link"><div>Sign up for free</div></Link>
             </div>
             <div className="main__container main__container--off-position">
-                <img className="main__container__image" src={quote2}/>
-                <h3>Convunity Landing Page</h3>
-                <p></p>
+                <img className="main__container__image" src={quote4} alt="quote bubble"/>
+                <h3>We know simple is better.</h3>
+                <p>Convunity works under the premise that humans are active and busy individuals. We need instant access to the things we like to do.</p>
+                <p>Convunity will showcase a lobby-type format in which the user will see available users ready to practice or ready to help others practice.</p>
                 <Link to='/register' className="link"><div>Sign up for free</div></Link>
             </div>
             <div className="main__container">
-                <img className="main__container__image" src={quote3}/>
-                <h3>Convunity Landing Page</h3>
-                <p></p>
+                <img className="main__container__image" src={quote3} alt="quote bubble"/>
+                <h3>Convunity in a glimpse...</h3>
+                <p>We are an easy-to-use video chat app.</p>
+                <p>We are not a beginners app. Basic conversational skills are required.</p>
                 <Link to='/register' className="link"><div>Sign up for free</div></Link>
             </div>
         </section>
