2298397b725a115f52d9fba684285f33c15687b9
Fernando Matamoros <fmatamoros202@gmail.com>
2021-12-04 13:02:49 -0500

    Addded preliminary styles to the register page

--- client/src/Components/Dashboard/Dashboard.jsx
+++ client/src/Components/Dashboard/Dashboard.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
+import './Dashboard.scss';
 import {Link} from 'react-router-dom';
+import logo from '../../assets/logo/logo-main.png'
+import logoGirl from '../../assets/images/logo-girl.png';
+
 
 function Dashboard(props) {
 
     return (
-        <div>
-            <h1>Welcome User</h1>
-            <Link to='/chat'><div>Lobby</div></Link>
-        </div>
+                <>
+                <header className="dashboard-header">
+                    <div>
+                        <Link to="/">
+                            <img src={logo} className="dashboard-logo" alt="convunity logo"/>
+                        </Link>
+                    </div>
+                </header>
+                <div className="dashboard-main">
+                    <h1 className="dashboard-main__title">Welcome User</h1>
+                    <div className="dashboard-main__container">
+                        <img className="dashboard-main__container__image" src={logoGirl} alt="A woman chatting on her phone"></img>
+                        <div className="dashboard-main__container__info">
+                            <p className ="dashboard-subtext">Please provide the following information:</p>
+                            <Link className="link" to='/chat'><div>Lobby</div></Link>
+                        </div>
+                    </div>
+                </div>
+                <footer className="footer">
+    
+                </footer>
+            </>
     );
 }
 
--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -9,7 +9,7 @@ function Register(props) {
         e.preventDefault();
         console.log(e);
         console.log(props.match.path)
-        props.history.replace('');
+        props.history.replace('/');
     }
     return (
         <>
@@ -25,25 +25,28 @@ function Register(props) {
                 <div className="register-main__container">
                     <img className="register-main__container__image" src={logoGirl} alt="A woman chatting on her phone"></img>
                     <div className="register-main__container__info">
-                        <p className ="register-subtext">Please provide the following information</p>
+                        <p className ="register-subtext">Please provide the following information:</p>
                         <form className ="register-form"onSubmit={onSubmit}>
                             {/* <label>Name</label> */}
-                            <input placeholder="Name"></input>
+                            <input className ="register-form__input" placeholder="UserName"></input>
                             {/* <label>Username</label> */}
-                            <input placeholder="UserName"></input>
+                            <input className ="register-form__input" placeholder="Password"></input>
                             {/* <label>Email</label> */}
-                            <input placeholder="Email"></input>
+                            <input className ="register-form__input" placeholder="Confirm Password"></input>
                             {/* <label>Nickname</label> */}
-                            <input placeholder="Nickname"></input>
+                            <input className ="register-form__input" placeholder="Email"></input>
                             {/* <label>Native Language</label> */}
-                            <input placeholder="Native Language"></input>
+                            <input className ="register-form__input" placeholder="Native Language"></input>
                             {/* <label>Learning Language</label> */}
-                            <input placeholder="Learning Language"></input>
+                            <input className ="register-form__input" placeholder="Learning Language"></input>
                             <button className ="register-button">Register</button>
                         </form>
                     </div>
                 </div>
             </div>
+            <footer className="footer">
+
+            </footer>
         </>
     );
 }
