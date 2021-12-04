ff4f030e6eda67e73aff7fbd092934d880ada3c1
Fernando Matamoros <fmatamoros202@gmail.com>
2021-12-04 08:34:14 -0500

    Added preliminary styles to the register page

--- client/src/Components/Register/Register.jsx
+++ client/src/Components/Register/Register.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import './Register.scss';
+import {Link} from 'react-router-dom';
+import logo from '../../assets/logo/logo-main.png'
+import logoGirl from '../../assets/images/logo-girl.png';
 
 function Register(props) {
     let onSubmit = (e)=>{
@@ -8,25 +12,39 @@ function Register(props) {
         props.history.replace('');
     }
     return (
-        <div>
-            <h1>Thank you for joining Convunity</h1>
-            <p>Please provide the following information</p>
-            <form onSubmit={onSubmit}>
-                <label>Name</label>
-                <input></input>
-                <label>Username</label>
-                <input></input>
-                <label>Email</label>
-                <input></input>
-                <label>Nickname</label>
-                <input></input>
-                <label>Native Language</label>
-                <input></input>
-                <label>Learning Language</label>
-                <input></input>
-                <button>Register</button>
-            </form>
-        </div>
+        <>
+            <header className="header">
+                <div>
+                    <Link to="/">
+                        <img src={logo} className="register-logo" alt="convunity logo"/>
+                    </Link>
+                </div>
+            </header>
+            <div className="register-main">
+                <h1 className="register-main__title">Thank you for joining Convunity!</h1>
+                <div className="register-main__container">
+                    <img className="register-main__container__image" src={logoGirl} alt="A woman chatting on her phone"></img>
+                    <div className="register-main__container__info">
+                        <p className ="register-subtext">Please provide the following information</p>
+                        <form className ="register-form"onSubmit={onSubmit}>
+                            {/* <label>Name</label> */}
+                            <input placeholder="Name"></input>
+                            {/* <label>Username</label> */}
+                            <input placeholder="UserName"></input>
+                            {/* <label>Email</label> */}
+                            <input placeholder="Email"></input>
+                            {/* <label>Nickname</label> */}
+                            <input placeholder="Nickname"></input>
+                            {/* <label>Native Language</label> */}
+                            <input placeholder="Native Language"></input>
+                            {/* <label>Learning Language</label> */}
+                            <input placeholder="Learning Language"></input>
+                            <button className ="register-button">Register</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
     );
 }
 
